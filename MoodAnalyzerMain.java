44a8ec127f9a5ca4453c6e5849cefbc3702a546c
sharale34 <sourabhharale8000@gmail.com>
2020-10-02 19:57:14 +0530

    Added: Welcome Meassage Is Added

---
+++ MoodAnalyzerMain.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz.MoodAnalyzer;
+
+public class MoodAnalyzerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Welcome To Mood Analyzer Problem");
+	}
+
+}
