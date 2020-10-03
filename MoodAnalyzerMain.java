15a5ba5f149ed100befa186e07b7b1bd9578b268
sharale34 <sourabhharale8000@gmail.com>
2020-10-03 07:53:51 +0530

    Refactor: Refactor The Code To Take Mood Message In Constructor

--- MoodAnalyzerMain.java
+++ MoodAnalyzerMain.java
@@ -1,10 +1,22 @@
 package com.BridgeLabz.MoodAnalyzer;
 
-import java.util.Scanner;
-
 public class MoodAnalyzerMain {
+    private String msg;
 	
-	public String analyseMood(String msg)
+	public MoodAnalyzerMain() {
+		
+	}
+	
+	public MoodAnalyzerMain(String msg) {
+		this.msg=msg;
+	}
+	
+	public String analyseMood(String msg) {
+		this.msg=msg;
+		return analyseMood();
+	}
+	
+	public String analyseMood()
 	{
 		if(msg.contains("Sad"))
 		{
@@ -15,14 +27,6 @@ public class MoodAnalyzerMain {
 			return "Happy";
 		}
 	}
-	public static void main(String[] args) {
-	// TODO Auto-generated method stub
-     System.out.println("Welcome To Mood Analyzer Problem");
-     Scanner sc = new Scanner(System.in);
-     String msg = sc.nextLine();
-     MoodAnalyzerMain Obj = new MoodAnalyzerMain();
-     String result = Obj.analyseMood(msg);
-	 System.out.println(result);
-	}
-
 }
+
+
